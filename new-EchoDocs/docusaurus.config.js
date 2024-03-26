62878df4408c03be52a70b3fd34042b87d2e1472
MaybeIzen <135663611+maybeizen@users.noreply.github.com>
2024-03-26 13:51:01 +0000

    Update docs config

--- new-EchoDocs/docusaurus.config.js
+++ new-EchoDocs/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://wiki.echoenterprises.net",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -43,14 +43,11 @@ const config = {
           sidebarPath: "./sidebars.js",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/echo-enterprises",
         },
         theme: {
           customCss: "./src/css/custom.css",
